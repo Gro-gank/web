0f8dd4bdb8e1b960849dc4ec852114e1365ddaa1
Gro-gank <grogankick@gmail.com>
2021-10-10 19:15:59 +0700

    up

--- ctfacademy-master/phpweb/ctfacademy-master/includes/header.php
+++ ctfacademy-master/phpweb/ctfacademy-master/includes/header.php
@@ -1,120 +1,114 @@
 <!DOCTYPE html>
-<html  lang="en">
-	<head>
-		<meta charset="utf-8" />
-		<meta name="viewport" content="width=device-width, initial-scale=1">
-			<title>CTF Academy : Home</title>
-			<meta name="description" content="">
-			
-			<meta name="author" content="CTF Academy">			
-			<link href="src/bootstrap/css/bootstrap.min.css" rel="stylesheet">	
-				<!--[if lt IE 9]>
+<html lang="en">
+
+<head>
+	<meta charset="utf-8" />
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>CTF Academy : Trang chủ</title>
+	<meta name="description" content="">
+	<meta name="author" content="CTF Academy">
+	<link href="src/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+	<!--[if lt IE 9]>
 				<script src="http://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv.min.js"></script>
 				<![endif]-->
-				<link rel="stylesheet" href="src/font-awesome-4.7.0/css/font-awesome.min.css" />				
-				<script src="src/jquery-3.4.1.min.js"></script>
-				<script src="src/popper.min.js"></script>				
-				<script src="src/bootstrap/js/bootstrap.min.js"></script>		
-				<link href="style.css" rel="stylesheet" />
-				<script src="src/jquery.md5.min.js"></script>
-				<script src="src/hashCheck.js"></script>
-				<script src="src/flagValidate.js"></script>
-				
+	<link rel="stylesheet" href="src/font-awesome-4.7.0/css/font-awesome.min.css" />
+	<script src="src/jquery-3.4.1.min.js"></script>
+	<script src="src/popper.min.js"></script>
+	<script src="src/bootstrap/js/bootstrap.min.js"></script>
+	<link href="style.css" rel="stylesheet" />
+	<script src="src/jquery.md5.min.js"></script>
+	<script src="src/hashCheck.js"></script>
+	<script src="src/flagValidate.js"></script>
+	<link rel="icon" href="src/HVKTMM.ico" />
+</head>
 
-				<link rel="icon" href="src/HVKTMM.ico" />
-		</head>				
-				
-				<body>
-				
-				
-				
-				<!-- Header -->
-				<header id="top">					
-					<div class="container-fluid ctfaLogo">
-				<div class="row">				
-					<div class="col-md-12">
-						<div class="myLogo ">						
-						<a href="index.php">						
-						<img src="src/HVKTMM.jpg" alt="CTFA Logo">											
+<body>
+	<!-- Header -->
+	<header id="top">
+		<div class="container-fluid ctfaLogo">
+			<div class="row">
+				<div class="col-md-12">
+					<div class="myLogo ">
+						<a href="index.php">
+							<img src="src/HVKTMM.jpg" alt="CTFA Logo">
 						</a>
-						</div>						
-						<div class="floatText">						
-						<h2 class="display-4">						
-						<span class = "mainblue">						
-						<a href="index.php">						
-						Cyber Training  
-											
-						<br class="break">						
-				
-						
-						Force Academy
-						</a>
-						</span>						
+					</div>
+					<div class="floatText">
+						<h2 class="display-4">
+							<span class="mainblue" style="font-size: 3rem;">
+								<a href="index.php">
+									Học Viện Đào Tạo
+
+									<br class="break">
+
+									Lực Lượng Không Gian Mạng
+								</a>
+							</span>
 						</h2>
-						</div>						
-						</div>
-						</div>						
-					  </div>			
-				</header>
-				
-<!-- Navigation -->
-<nav class="navbar navbar-expand-md navbar-dark"  id="myNav">
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-  <div class="collapse navbar-collapse justify-content-md-center" id="navbarNavAltMarkup">
-    <div class="navbar-nav">
-		<ul class="nav navbar-nav">
-			<li class="nav-item">
-				<a class="nav-link" href="index.php"><i class="fa fa-home homepageSize" aria-hidden="true"></i></a>
-			</li>
-			
-			<li class="nav-item">
-				<a class="nav-link" href="crypto/index.php">Cryptography</a>
-			</li>
-			
-			
-			
-			<li class="nav-item">
-				<a class="nav-link" href="osint/index.php">Open-Source Intel</a>
-			</li>
-			
-			<li class="nav-item">
-				<a class="nav-link" href="web/index.php">Web App Exploitation</a>
-			</li>
+					</div>
+				</div>
+			</div>
+		</div>
+	</header>
 
-			<li class="nav-item">
-				<a class="nav-link" href="network/index.php">Network Forensics</a>
-			</li>								
-		</ul>
-    </div>
-  </div>
-</nav>
+	<!-- Navigation -->
+	<nav class="navbar navbar-expand-md navbar-dark" id="myNav">
+		<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+			<span class="navbar-toggler-icon"></span>
+		</button>
+		<div class="collapse navbar-collapse justify-content-md-center" id="navbarNavAltMarkup">
+			<div class="navbar-nav">
+				<ul class="nav navbar-nav">
+					<li class="nav-item">
+						<a class="nav-link" href="index.php"><i class="fa fa-home homepageSize" aria-hidden="true"></i></a>
+					</li>
 
-							
-<!-- Main container -->
-<div class="container-fluid" >
+					<li class="nav-item">
+						<a class="nav-link" href="crypto/index.php">Cryptography</a>
+					</li>
 
-<div id="myBody">
-  <div class="row">						
 
-<!-- Column Navigation -->
-<div class="col-md-3 order-2 order-md-1" >
 
-<div class="list-group-flush myLightBox mb-4">
-  <div class="list-group-item contents" >Table of Contents</div>
-    <a href="other/about_us.php" class="list-group-item list-group-item-action">About Us</a>
-	<a href="other/contact_us.php" class="list-group-item list-group-item-action">Contact Us</a>
-	<a href="other/glossary.php" class="list-group-item list-group-item-action">Glossary</a>
-	<a href="other/quick_start.php" class="list-group-item list-group-item-action">Quick Start Guide</a>
-	<a href="other/ethics.php" class="list-group-item list-group-item-action">Cybersecurity Ethics</a>
-	<a href="other/enable_javascript.php" class="list-group-item list-group-item-action">Enable JavaScript</a>
-	<a href="other/privacy-policy.php" class="list-group-item list-group-item-action">Privacy Policy</a>
-	<a href="other/legal_disclaimer.php" class="list-group-item list-group-item-action">Legal Disclaimer</a>
-	<a href="other/terms-of-use.php" class="list-group-item list-group-item-action">Terms of Use</a>
+					<li class="nav-item">
+						<a class="nav-link" href="osint/index.php">Open-Source Intel</a>
+					</li>
 
-</div>
+					<li class="nav-item">
+						<a class="nav-link" href="web/index.php">Web App Exploitation</a>
+					</li>
 
-</div>
+					<li class="nav-item">
+						<a class="nav-link" href="network/index.php">Network Forensics</a>
+					</li>
+				</ul>
+			</div>
+		</div>
+	</nav>
 
-<!-- End column -->	
+
+	<!-- Main container -->
+	<div class="container-fluid">
+
+		<div id="myBody">
+			<div class="row">
+
+				<!-- Column Navigation -->
+				<div class="col-md-3 order-2 order-md-1">
+
+					<div class="list-group-flush myLightBox mb-4">
+						<div class="list-group-item contents">Table of Contents</div>
+						<a href="other/about_us.php" class="list-group-item list-group-item-action">About Us</a>
+						<a href="other/contact_us.php" class="list-group-item list-group-item-action">Contact Us</a>
+						<a href="other/glossary.php" class="list-group-item list-group-item-action">Glossary</a>
+						<a href="other/quick_start.php" class="list-group-item list-group-item-action">Quick Start Guide</a>
+						<a href="other/ethics.php" class="list-group-item list-group-item-action">Cybersecurity Ethics</a>
+						<a href="other/enable_javascript.php" class="list-group-item list-group-item-action">Enable JavaScript</a>
+						<a href="other/privacy-policy.php" class="list-group-item list-group-item-action">Privacy Policy</a>
+						<a href="other/legal_disclaimer.php" class="list-group-item list-group-item-action">Legal Disclaimer</a>
+						<a href="other/terms-of-use.php" class="list-group-item list-group-item-action">Terms of Use</a>
+
+					</div>
+
+				</div>
+
+				<!-- End column -->
