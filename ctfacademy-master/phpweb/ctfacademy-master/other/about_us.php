8b89470ca0cc9002d4fa6906abd352253c246798
Gro-gank <grogankick@gmail.com>
2021-11-30 20:00:00 +0700

    update about us

--- ctfacademy-master/phpweb/ctfacademy-master/other/about_us.php
+++ ctfacademy-master/phpweb/ctfacademy-master/other/about_us.php
@@ -23,20 +23,24 @@ Gặp gỡ các thành viên của nhóm ACT CTF.
 </p>
 
 <p id="jacob">
-<b>Jacob Corley</b> is pursuing a bachelor’s degree in IT with a concentration in cybersecurity at Middle Georgia State University. He is also an EC-Council Certified Ethical Hacker and Certified Threat Intelligence Analysis. Jacob has competed in several cybersecurity CTF competitions where he placed 21st individually in the nation, and as leader the MGA CyberKnights, he led a team that placed 12th. Jacob used his skills as a certified cybersecurity specialist to develop lessons and challenges for the CTF Academy training program.
-</p>
-
+<b>Cổ Ngọc Minh Huy</b> MSSV: AT150721 </p>
 <p id="james">
-<b>James Corley</b> is pursuing a bachelor’s degree in IT with a concentration in web applications development at Middle Georgia State University. He has had experience developing webpages and web applications for different academic projects. James recently won a Certificate of Academic Excellent for his concentration in Web Applications Development. James used his experience with Bootstrap, jQuery, and JavaScript to create CTFA’s responsive layout and interactive challenges.
-</p>
-
+<b>Phạm Đăng Thắng</b> MSSV: AT150745</p>
 <p id="john">
-<b>John Reiman</b> has over 30 years of security, military, and law enforcement experience. Most of this time has been spent as a Facility Security Officer supporting classified contracts for a variety of Government entities. He has also worked as an Information System Security Officer (ISSO) and Information System Security Manager (ISSM), Police Officer, Military Police Officer, and combat veteran. Mr. Reiman has degrees in Management, Criminal Justice, and in 2019 will graduate from Middle Georgia State University with a CyberSecurity degree.
+<b>Phạm Ngọc Luân</b> MSSV: AT141031</p>
+
+<b>Học viện Kỹ Thuật Mật Mã</b>
+Học viện Kỹ thuật Mật mã (tiếng Anh: Vietnam Academy of Cryptography Techniques) là một trường đại học công lập trực thuộc Ban Cơ yếu Chính phủ, được thành lập ngày 17 tháng 2 năm 1995 có chức năng đào tạo cán bộ có trình độ đại học, sau đại học và nghiên cứu khoa học kỹ thuật mật mã của ngành Cơ yếu Việt Nam. Học viện cũng được chính phủ Việt Nam lựa chọn là một trong tám cơ sở trọng điểm đào tạo nhân lực an toàn thông tin Việt Nam theo Đề án đào tạo và phát triển nguồn nhân lực an toàn, an ninh thông tin đến năm 2020
 </p>
-
-
-
-
+<p>
+- Học viện Kỹ thuật Mật mã, tiền thân là Trường Cán bộ Cơ yếu Trung ương (ngày 15 tháng 4 năm 1976), Trường Đại học Kỹ thuật Mật mã (ngày 5 tháng 6 năm 1985) và Viện Nghiên cứu Khoa học Kỹ thuật Mật mã (ngày 17 tháng 2 năm 1980), được thành lập ngày ngày 17 tháng 2 năm 1995 trên cơ sở sáp nhập Trường Đại học Kỹ thuật Mật mã và Viện Nghiên cứu Khoa học Kỹ thuật Mật mã.
+</p>
+<p>
+- Học viện là cơ sở duy nhất của Việt Nam có chức năng đào tạo cán bộ có trình độ đại học, sau đại học và nghiên cứu khoa học kỹ thuật mật mã của ngành Cơ yếu Việt Nam. Đây còn là nơi đào tạo và bồi dưỡng đội ngũ cán bộ cơ yếu, sĩ quan chỉ huy kỹ thuật mật mã cho quân đội Việt Nam, Lào, Campuchia và Cuba.
+</p>
+<p>
+- Hiện nay Học viện đào tạo thêm đại học chính quy hệ dân sự ngành CNTT (chuyên ngành An toàn thông tin, chuyên ngành lập trình nhúng và di động) và ngành Điện tử viễn thông (chuyên ngành hệ thống nhúng và điều khiển tự động), Tiến sĩ ngành Mật mã và Thạc sĩ chuyên ngành An toàn thông tin.
+</p>
 			
 			<div id="backToTop" class="my-3">
 <a href="#top">Về đầu trang</a>
